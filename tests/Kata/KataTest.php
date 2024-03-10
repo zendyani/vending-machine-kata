b7bc091d7f1387d687da56c860c48826cbcf6427
Jalil <zendyani@gmail.com>
2024-03-10 13:30:21 +0100

    Add phpunit

---
+++ tests/Kata/KataTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Kata\Test;
+
+use Kata\Kata;
+use PHPUnit\Framework\TestCase;
+
+class KataTest extends TestCase
+{
+    public function testWork()
+    {
+        $kata = new Kata();
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+//
+// PHPUnit bootstrap file
+//
+
+// Require the composer global autoloader
+require_once __DIR__.'/../vendor/autoload.php';
